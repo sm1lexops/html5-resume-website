7bcfb5b96fe53badbf6d2ad834e463166b923f13
sm1lexops <smiledevops@gmail.com>
2024-01-08 18:09:02 +0000

    add lambda function url

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("resume-visitors-counter");
+    let response = await fetch("https://4r3vturzxat2vkxajjemqcyr3e0hlcyj.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
